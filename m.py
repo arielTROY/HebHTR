632be099782b30f81c896823cbd1b19c9e4a486f
arielT <59286427+arielTROY@users.noreply.github.com>
2023-07-03 14:37:03 +0300

    Update m.py

--- m.py
+++ m.py
@@ -1,9 +1,9 @@
 from HebHTR import *
 from flask import *
 import json
-from io import BytesIO
 import requests
 from PIL import Image
+from io import BytesIO
 
 app = Flask(__name__)
 
@@ -16,19 +16,28 @@ def home_page():
 @app.route('/detect', methods=['GET'])
 def detect():
     img_url = request.args.get('img')
-    
+
     # Fetch image from the URL
     response = requests.get(img_url)
     image = Image.open(BytesIO(response.content))
-    
-    # Create HebHTR object and process image
-    htr = HebHTR(image)
+
+    # Generate a unique filename for saving the image
+    filename = f"temp_image_{time.time()}.jpg"
+
+    # Save the image locally
+    image.save(filename)
+
+    # Create HebHTR object and process the saved image
+    htr = HebHTR(filename)
     text = htr.imgToWord(iterations=5, decoder_type='best_path')
-    
+
     txt = str(text)
     txt = txt[2:]
     print(txt)
-    
+
+    # Remove the temporary image file
+    os.remove(filename)
+
     data_set = {'page': 'home', 'recognized': txt}
     json_dump = json.dumps(data_set)
     return json_dump
