e89d400ad717ecb15e9bb9419184c5fe46919968
arielT <59286427+arielTROY@users.noreply.github.com>
2023-07-03 13:40:23 +0300

    Update m.py

--- m.py
+++ m.py
@@ -1,12 +1,33 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 from HebHTR import *
+from flask import *
+import json, time
+import os
+from PIL import Image
+
 
 # Create new HebHTR object.
-img = HebHTR('ex.png')
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home_page():
+    data_set ={'page': 'home', 'recognized': 'use the endpoint /detect '}
+    json_dump = json.dumps(data_set)
+    return json_dump
+
+@app.route('/detect', methods=['GET'])
+def detect():
+    im = request.args.get('img')
+    image = HebHTR(im)
 
 # Infer words from image.
-text = img.imgToWord(iterations=5, decoder_type='best_path')
-txt=str(text)
-txt=txt[2:]
-print(txt)
+    text = img.imgToWord(iterations=5, decoder_type='best_path')
+    txt=str(text)
+    txt=txt[2:]
+    print(txt)    
+    data_set ={'page': 'home', 'recognized': txt}
+    json_dump = json.dumps(data_set)
+    return json_dump
+
+if __name__ == '__main__':
+    app.run(port=7777)
