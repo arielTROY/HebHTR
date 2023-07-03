a5aab89336ca5b40fc6e105329a968f054fbe4b0
arielT <59286427+arielTROY@users.noreply.github.com>
2023-07-03 14:21:27 +0300

    Update m.py

--- m.py
+++ m.py
@@ -1,31 +1,34 @@
 from HebHTR import *
 from flask import *
-import json, time
-import os
+import json
+import requests
 from PIL import Image
-
-
-# Create new HebHTR object.
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home_page():
-    data_set ={'page': 'home', 'recognized': 'use the endpoint /detect '}
+    data_set = {'page': 'home', 'recognized': 'use the endpoint /detect '}
     json_dump = json.dumps(data_set)
     return json_dump
 
 @app.route('/detect', methods=['GET'])
 def detect():
-    im = request.args.get('img')
-    image = HebHTR(im)
-
-# Infer words from image.
-    text = image.imgToWord(iterations=5, decoder_type='best_path')
-    txt=str(text)
-    txt=txt[2:]
-    print(txt)    
-    data_set ={'page': 'home', 'recognized': txt}
+    img_url = request.args.get('img')
+    
+    # Fetch image from the URL
+    response = requests.get(img_url)
+    image = Image.open(BytesIO(response.content))
+    
+    # Create HebHTR object and process image
+    htr = HebHTR(image)
+    text = htr.imgToWord(iterations=5, decoder_type='best_path')
+    
+    txt = str(text)
+    txt = txt[2:]
+    print(txt)
+    
+    data_set = {'page': 'home', 'recognized': txt}
     json_dump = json.dumps(data_set)
     return json_dump
 
